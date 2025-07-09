4ca6b46e893e04631c5915bbececd4735b755419
sunflower1729 <126437326+sunflower1729@users.noreply.github.com>
2025-07-09 09:55:36 +0900

    [level 1] Title: 최소직사각형, Time: 0.64 ms, Memory: 5.06 MB -BaekjoonHub

--- C++/프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ C++/프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,48 +1,17 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-void swap(vector<int> &a){
-    int temp = a[0];
-    a[0] = a[1];
-    a[1] = temp;
-}
-
 int solution(vector<vector<int>> sizes) {
-    int answer = 0;
-    int max = 0;
+    int max_l = max(sizes[0][0],sizes[0][1]);
+    int min_l = min(sizes[0][0],sizes[0][1]);
     
-    for(auto &a : sizes){
-        if(max < a[0]) {
-            max = a[0];
-            swap(a);
-        }
-        if(max < a[1])
-            max = a[1];
-    }
-    sort(sizes.rbegin(), sizes.rend());
-    
-    for(auto a : sizes){
-        cout << a[0] << " " << a[1] << endl;
+    for(int i = 1; i<sizes.size(); i++){
+        max_l = max(max(sizes[i][0],sizes[i][1]), max_l);
+        min_l = max(min(sizes[i][0],sizes[i][1]), min_l);
     }
     
-    int max2 = 0;
-    //answer = sizes[0][0] * sizes[0][1];
-    for(auto a : sizes){
-        cout << "\n" << a[0] << " " << a[1] << endl;
-        if( a[0] > a[1]){
-            swap(a);
-            if(max2 < a[0])
-                max2 = a[0];
-        }
-        else{
-            if(max2 < a[0])
-                max2 = a[0];
-            answer = max2 * max;
-            break;
-        }
-    }
-    return answer;
-}                                        
+    return max_l*min_l;
+}
