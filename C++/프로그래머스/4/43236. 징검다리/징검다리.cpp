a90f8c0396db9fa5830c3143727f58ec113db57b
sunflower1729 <126437326+sunflower1729@users.noreply.github.com>
2025-07-15 14:24:44 +0900

    [level 4] Title: 징검다리, Time: 8.87 ms, Memory: 5.55 MB -BaekjoonHub

---
+++ C++/프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+    vector<int> rock_dis;
+    int answer = 0;
+    
+    sort(rocks.begin(), rocks.end());
+    
+    rock_dis.push_back(rocks[0]);
+    for(int i = 1; i < rocks.size(); i++){
+        rock_dis.push_back(rocks[i] - rocks[i-1]);
+    }    
+    rock_dis.push_back(distance - rocks.back());
+    
+    int left = 1;
+    int right = distance;
+    
+    while(left <= right){
+        
+        int mid = (right + left) / 2;
+        vector<int> d;
+        
+        int temp = 0;
+        int erase = 0;
+        for(int i = 0; i < rock_dis.size(); i++){
+            temp += rock_dis[i];
+            if(temp < mid){
+                erase++;
+            }
+            else{
+                d.push_back(temp);
+                temp = 0;
+            }
+        }
+        
+        if(temp != 0){
+            d.push_back(temp);
+        }
+        
+        if(erase > n){
+            right = mid - 1;
+        }
+        else{
+            left = mid + 1;
+            answer = mid;
+        }
+    }
+    
+    return answer;
+}
